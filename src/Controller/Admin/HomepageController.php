e824ea6de5b05d0241ec4bd32e841608617bdecb
Your Name <you@example.com>
2019-01-18 00:12:48 +0100

    user connexion

---
+++ src/Controller/Admin/HomepageController.php
@@ -0,0 +1,17 @@
+<?php 
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/** @Route("/admin") */
+class HomepageController extends Controller {
+
+    /**
+     * @Route("/")
+     */
+    public function index() {
+        return $this->render('admin/homepage/index.html.twig', ['mainNavAdmin' => true, 'title' => 'Espace Admin']);
+    }
+
+}
